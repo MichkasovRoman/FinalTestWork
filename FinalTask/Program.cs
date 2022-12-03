9616ee85166baf77c81035bb5ad79dab1753b102
Michkasov Roman <mihjob@list.ru>
2022-12-04 00:19:55 +0300

    Wrote the first method GetStringArray

--- FinalTask/Program.cs
+++ FinalTask/Program.cs
@@ -2,4 +2,13 @@
 // длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
 // либо задать на старте выполнения алгоритма.
 
-
+string[] GetStringArray(int size)
+{
+    string[] stringArray = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine($"Введите {i}-й элемент массива: ");
+        Console.ReadLine(stringArray[i]);
+    }
+    return stringArray;
+}
